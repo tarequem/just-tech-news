ee383e8f81fcd0a970a6d3a05a4c720ee7a42b5a
tarequem <moatartareque@gmail.com>
2022-08-01 20:10:50 -0400

    refactored vote route

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -72,30 +72,13 @@ router.post('/', (req, res) => {
 
 // PUT /api/posts/upvote
 router.put('/upvote', (req, res) => {
-  Vote.create({
-    user_id: req.body.user_id,
-    post_id: req.body.post_id
-  })
-  .then(() => {
-    // then find the post we just voted on
-    return Post.findOne({
-      where: {
-        id: req.body.post_id
-      },
-      attributes: [
-        'id',
-        'post_url',
-        'title',
-        'created_at',
-        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-      ],
-    })
-    .then(dbPostData => res.json(dbPostData))
+  // custom static method created in models/Post.js
+  Post.upvote(req.body, { Vote })
+    .then(updatedPostData => res.json(updatedPostData))
     .catch(err => {
       console.log(err);
       res.status(400).json(err);
     });
-  });
 });
 
 router.put('/:id', (req, res) => {
